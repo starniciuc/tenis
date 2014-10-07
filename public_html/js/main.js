e7d5c7a21d9f626d816944c908b0c8f8fd52d93c
Mihai <Mihai@Mihai.Dlink>
2014-10-07 10:00:59 +0300

    all slider work!!!

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,44 +1,12 @@
-if ($('.tennis-slide').length > 0) {
-    $('.tennis-slide').bxSlider({
-        infiniteLoop: true,
-        nextSelector: '#slider-next',
-        prevSelector: '#slider-prev',
-        nextText: "",
-        prevText: "",
-        pager: true,
-        adaptiveHeight: true
-    });
+function slideModal() {
+    $("#sliderModal").css("display","block");
+    $(".submodal").css("display","block");
 }
-if ($('.gallery-slide').length > 0) {
-$('.gallery-slide').bxSlider({
-    nextSelector: '#slider-next',
-    prevSelector: '#slider-prev',
-    pager: false,
-    minSlides: 3,
-    maxSlides: 3,
-    nextText: "",
-    prevText: "",
-    slideMargin: 10,
-    slideWidth: 230,
-    adaptiveHeight: true
+function closeModal() {
+    $("#sliderModal").css("display","none");
+    $(".submodal").css("display","none");
+}
+$(".submodal").click(function(){
+    $("#sliderModal").css("display","none");
+    $(".submodal").css("display","none");
 });
-}
-var mSlide = " ";
-if ($('#sliderModal').length > 0) {
-    mSlide = $('.bxslider').bxSlider({
-        nextSelector: '#slider-next1',
-        prevSelector: '#slider-prev1',
-        pager: false,
-        nextText: "",
-        prevText: "",
-        adaptiveHeight: true,
-        oneToOneTouch: true
-    });
-}
-$('#sliderModal').on('shown.bs.modal', function () {
-    mSlide.reloadSlider();
-});
-function slideModal(a) {
-    $("#sliderModal").modal("show");
-    mSlide.goToSlide(a);
-}
